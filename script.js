52ddfeca2e12d5203040e7c86a07803cf01e4035
FrantzCFelix <Frantz.C.Felix@gmail.com>
2019-12-21 00:15:49 -0800

    Added timer function from old function.js file and changed it

--- script.js
+++ script.js
@@ -3,13 +3,57 @@
 
 /*******var decleration******************/
 var timerDisplay = $('#time-display');
-var startButton = $('#start-button')
+var startButton = $('#start-button');
+var timeRemaining = questions.length * 15;
+var timerId;
+var questionIndex = 0;
+ 
 
 /****************************************/
-startButton.on("click",function () {
+startButton.on("click", startGame);
+function startGame() {
+
     $("#start-screen").hide();
-    startTimer(4, timerDisplay)
-});
+
+    timerId = setInterval(timer, 1000);
+    
+}
+
+// generateQuestion()
+// question.title[0]
+// questionIndex++
+// if (questions.length === questionIndex) {gameOver()} else {generateQuestion[questionIndex]}
+
+// clickButton() 
+// valdiate answer (right or wrong [if wrong(time -= 10)])
+// use array.find?
+//for each
+//array.some
+// generateQuestion()
+
+// gameOver()
+
+
+
+
+// WrongAnswer()
+
+
+
+
+function timer() {
+
+    --timeRemaining;
+
+    timerDisplay.text(timeRemaining);
+git
+    if (timeRemaining < 0) {
+        
+        // GameOver
+        clearInterval(timerId)
+    }
+}
+
 
 
 
