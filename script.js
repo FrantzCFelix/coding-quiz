2ac2c2c473bdbf9e7e5d60f650bf9717a75fcddc
FrantzCFelix <Frantz.C.Felix@gmail.com>
2019-12-21 06:59:28 -0800

    Updated generateQuestion function to display correct obj in questions array. validate answer and subtract points for bad answers

--- script.js
+++ script.js
@@ -7,73 +7,118 @@ var startButton = $('#start-button');
 var timeRemaining = questions.length * 15;
 var timerId;
 var questionIndex = 0;
- 
+var score = 0;
+var quizDiv = $("#quiz");
+
 
 /****************************************/
-startButton.on("click", function(){
+startButton.on("click", function () {
 
-    startGame()
+    startGame();
     generateQuestion(questions);
+    
 
 
 });
 
 
-function generateQuestion(questionsArr)
-{
-    var quizDiv = $("#quiz");
-    var displayedQuestion = $("<h1></h1>")
+
+
+
+//SubmitScore()
+// if (questions.length === questionIndex) {gameOver()
+//function gameOver()
+//{
+//    if(timeRemaining)
+    //submitScore()
+//}
+//getHighscore()
+//clearHighScore()
+
+
+
+
+function generateQuestion(questionsArr) {
+    
+    var displayedQuestion = $("<h1></h1>");
+
+    displayedQuestion.addClass("text-center");
     displayedQuestion.text(questionsArr[questionIndex].title);
     quizDiv.append(displayedQuestion);
-    
-    for(var i = 0; i< questionsArr[questionIndex].choices.length;i++)
-    {
-        console.log(questionsArr[questionIndex].choices.length);
-        var choice = $("<a>----------------</a>");
+
+    for (var i = 0; i < questionsArr[questionIndex].choices.length; i++) {
+
+        var choice = $("<a></a>");
+        choice.attr("href", "#");
+        choice.addClass("list-group-item list-group-item-action choice-button");
+        choice.attr("id", i);
+        //combine attrs^
+        choice.text(questionsArr[questionIndex].choices[i]);
         quizDiv.append(choice);
 
-
-
     }
-
-
-    
-
-
-questionIndex++;
+    valdiateAnswer(questionsArr)
 }
 
+function valdiateAnswer(questionArr)
+{
+    var choiceButtons = $(".choice-button");
+    choiceButtons.on("click", function () {
+        var choiceButtonPicked = $(event.target).attr("id");
 
 
+        if ((questionArr[questionIndex].choices[choiceButtonPicked] === questionArr[questionIndex].answer)&&questionIndex < questionArr.length) {
+            var hrBar = $("<hr>");
+            hrBar.text("Correct!");
+            hrBar.addClass("span-quiz text-center m-auto py-3 badge badge-success w-50")
+            quizDiv.append(hrBar);
 
-// question.title[0]
-// questionIndex++
-// if (questions.length === questionIndex) {gameOver()} else {generateQuestion[questionIndex]}
+            //to display correct/wrong before wipe
+            //but now able to mash buttons
+            setTimeout(function () {
+                quizDiv.empty();
+            }, 1000);
 
-// clickButton() 
-// valdiate answer (right or wrong [if wrong(time -= 10)])
-// use array.find?
-//for each
-//array.some
-// generateQuestion()
+            questionIndex++;
 
-// gameOver()
+            setTimeout(function () {
+                generateQuestion(questions)
+            }, 1000);
+        }
+        else{
+            var hrBar = $("<hr>");
+            hrBar.text("Wrong!");
+            hrBar.addClass("span-quiz text-center m-auto py-3 badge badge-danger w-50")
+            quizDiv.append(hrBar);
+
+             //to display correct/wrong before wipe
+             setTimeout(function () {
+                quizDiv.empty();
+            }, 1000);
+
+            questionIndex++;
+            timeRemaining-=10;
+
+            setTimeout(function () {
+                generateQuestion(questions)
+            }, 1000);
+
+        }
 
 
+       
 
-
-// WrongAnswer()
+    });
+    
+}
 
 function startGame() {
 
     $("#start-screen").hide();
 
-     timerId = setInterval(timer, 1000);
-    
+    timerId = setInterval(timer, 1000);
+
 }
-
-
-
 
 function timer() {
 
@@ -82,9 +127,9 @@ function timer() {
     timerDisplay.text(timeRemaining);
 
     if (timeRemaining <= 0) {
-        
+
         // GameOver
-        clearInterval(timerId)
+        clearInterval(timerId);
     }
 }
 
